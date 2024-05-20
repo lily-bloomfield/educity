fcf455831ffb27afa6a42d6aa643f96735d962e3
lily-bloomfield <lilybloomfield.lb@gmail.com>
2024-05-20 14:31:49 +0630

    photo-change

--- Educity/src/Components/Campus/Campus.jsx
+++ Educity/src/Components/Campus/Campus.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import "./Campus.css";
 import Title from "../Title/Title";
-import gallery1 from "../../assets/gallery-1.png";
-import gallery2 from "../../assets/gallery-2.png";
-import gallery3 from "../../assets/gallery-3.png";
-import gallery4 from "../../assets/gallery-4.png";
+import gallery1 from "../../assets/campus-1.jpg";
+import gallery2 from "../../assets/campus-2.webp";
+import gallery3 from "../../assets/campus-3.jpg";
+import gallery4 from "../../assets/campus-4.webp";
 
 const Campus = () => {
   return (
     <div className="container campus-container">
       <Title title="Campus Photos" subTitle="GALLERY" />
       <div className="campus-img-container">
-        <img src={gallery1} />
-
-        <img src={gallery2} />
-        <img src={gallery3} />
-        <img src={gallery4} />
+        <div className="img-div">
+          <img src={gallery1} />
+        </div>
+        <div className="img-div">
+          <img src={gallery2} />
+        </div>
+        <div className="img-div">
+          <img src={gallery3} />
+        </div>
+        <div className="img-div">
+          <img src={gallery4} />
+        </div>
       </div>
       <button className=" btn-campus">
         Explore more <i class="fa-solid fa-arrow-right"></i>
