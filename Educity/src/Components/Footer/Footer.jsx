cffc476ebb923d83adba93071bae0da990cb653a
lily-bloomfield <lilybloomfield.lb@gmail.com>
2024-05-26 14:11:54 +0630

    1stcommit

--- Educity/src/Components/Footer/Footer.jsx
+++ Educity/src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>© 2024 Edusity. All rights reserved.</p>
+      <p>© 2024 Educity. All rights reserved.</p>
       <ul>
         <li>Terms of Services</li>
         <li>Privacy Policy</li>
